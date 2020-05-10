a1a408f0ebb70518393a9fcba8f6c9ce7bd93c01
Nishakant Pandey <nishakant.fsd@gmail.com>
2020-05-10 15:32:44 +0530

    Committing  Demo

---
+++ src/main/java/com/demo/gcloud/helloworld/demogcloudhelloworld/DemoGcloudHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.demo.gcloud.helloworld.demogcloudhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGcloudHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGcloudHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/gcloud/helloworld/demogcloudhelloworld/DemoGcloudHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.gcloud.helloworld.demogcloudhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGcloudHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
